57ba7eace9efa258aae13e175fc1b616a1499f6b
Ann-Marie <anniedillier@gmail.com>
2021-01-25 20:55:03 -0800

    redirect

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,7 +6,7 @@ function Nav() {
   return (
     <header className="flex-row px-1">
       <h2> 
-      <NavLink to="/about">
+      <NavLink to="/">
           <span role="img" aria-label="laptop">
             {" "}
             Ann-Marie Orozco 💻
